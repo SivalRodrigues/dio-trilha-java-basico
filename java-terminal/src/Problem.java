02e7b4a465a1a8866a51b0eb06e7df06aefa7efa
SivalRodrigues <sivaldesigne@gmail.com>
2025-01-16 15:15:31 -0300

    meu projeoto conta banco

---
+++ java-terminal/src/Problem.java
@@ -0,0 +1,18 @@
+
+    import java.util.Scanner;
+
+public class Problem {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, PROD;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		 //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+		PROD = 3 * 9;   
+
+		System.out.println("PROD = " + PROD);
+	}
+}
+
